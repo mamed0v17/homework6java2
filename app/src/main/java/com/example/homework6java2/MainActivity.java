f3ce4ecf6d04ef7272b21f28752c5708a263cbb4
mamed0v17 <rus.mamedov.2003002@mail.ru>
2021-10-20 17:30:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework6java2/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.homework6java2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.ContextMenu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.GenericArrayType;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText EditText_email, EditText_password;
+    private Button button;
+    private TextView textView;
+
+
+    String email = "admin@gmail.com";
+    String password = "admin";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText_email = findViewById(R.id.email);
+        EditText_password = findViewById(R.id.password);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.Register);
+        button.setOnClickListener(view -> {
+                    ;
+                    if (EditText_email.getText().toString().equals(email) && EditText_password.getText().toString().equals(password)) {
+                        EditText_password.setVisibility(View.GONE);
+                        EditText_email.setVisibility(View.GONE);
+                        button.setVisibility(View.GONE);
+                        textView.setVisibility(View.GONE);
+                        Toast.makeText(this, "Вы успешно вошли!", Toast.LENGTH_LONG).show();
+
+                    } else if (EditText_email.getText().toString().isEmpty() && EditText_password.getText().toString().isEmpty()) {
+                        Toast.makeText(this,"Поля пустые, Пожалуйста ввидите данные!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Неправильные данные!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+EditText_email.addTextChangedListener(new TextWatcher() {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if (EditText_email.getText().toString().isEmpty()){
+            ;
+        button.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.dc));
+    }else {
+            button.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.orange));
+
+        }
+    }
+});
+EditText_password.addTextChangedListener(new TextWatcher() {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if (EditText_password.getText().toString().isEmpty()){
+
+            button.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.dc));
+        }else {
+            button.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.orange));
+        }
+
+    }
+});
+
+           this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+    }
+
+
+
